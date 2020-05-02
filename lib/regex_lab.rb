32dbf5ace39a166883df08a25ccca586bf2b4f22
Janis Lebron <janislebron@gmail.com>
2020-05-02 23:05:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  "Hi I would like to eat an apple!".scan(\/^[aeiou])
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  "Understanding, where I come from is important."
 end
 
 def words_five_letters_long(text)
-
+  "These tests are so important for my learning."
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  "NO! I will not clip your toenails!"
 end
 
 def valid_phone_number?(phone)
-
+  "9174424640"
 end
